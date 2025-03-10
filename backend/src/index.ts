7a6c81c481d1e2a750c5cb85b868f466818cff57
Damim_X_Adi <imadeddinekir95@gmail.com>
2025-03-10 01:52:18 +0100

    Monday Mar/10/2025 Damim_X_Adi 01:52

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "../routes/auth.Routes.js";
 import messageRoutes from "../routes/message.Routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 
 //app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
